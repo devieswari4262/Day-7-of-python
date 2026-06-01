61aa441ae9085a0301cf07a81d27030ad1bcc594
devi eswari <vanapallidevieswari@gmail.com>
2026-06-01 16:17:01 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,81 @@
+#Loops
+#for loop
+
+'''for i in range (10):
+    print("apple")
+o/p:apple
+apple
+apple
+apple
+apple
+apple
+apple
+apple
+apple
+apple'''
+
+'''for i in range (10,15):
+    print("apple")
+o/p:apple
+apple
+apple
+apple
+apple
+
+for i in range(10,20,2):
+    print("apple")
+o/p:apple
+apple
+apple
+apple
+apple
+
+for i in range(10):
+    print(i)
+o/p:0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+fruit="mango"
+for i in range(10):
+    print(fruit)
+o/p:mango
+mango
+mango
+mango
+mango
+mango
+mango
+mango
+mango
+mango
+
+for i in range(10,0,-1):
+    print(i)
+o/p:10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+for i in range (1,10,4):
+    print(i)
+o/p:1
+5
+9'''
+
+a="mango"
+for i in  a:
+    print(i)
